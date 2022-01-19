cd18005fa5304897802b2c2b31766a4360f7b3e0
melicaso1624 <melicarmona35@gmail.com>
2022-01-18 23:21:57 -0500

    Servidor express

---
+++ express/contenedor.js
@@ -0,0 +1,73 @@
+const fs = require('fs');
+
+class Container {
+
+    /** Se guardan los objetos en el archivo */
+    save(product) {
+        let content = fs.readFileSync('./file/producto.txt', 'utf-8');
+        let array_product;
+        if (content) {
+            array_product = JSON.parse(content)
+        } else {
+            array_product = [];
+        }
+        array_product.push(product);
+        array_product.forEach((value, index) => {
+            value.id = index + 1;
+        });
+        let array_product_string = JSON.stringify(array_product)
+        fs.writeFileSync('./file/producto.txt', array_product_string)
+        console.log('Se guardo la información')
+    }
+
+    /** Se obtiene un elemento por el ID */
+    getById(id) {
+        let content = fs.readFileSync('./file/producto.txt', 'utf-8');
+        let array_product;
+        if (content) {
+            array_product = JSON.parse(content)
+        }
+        let product_filter = array_product.find(element => element.id == id)
+        console.log(product_filter);
+    }
+
+    /** Se obtiene toda la información */
+    getAll() {
+        let content = fs.readFileSync('./file/producto.txt', 'utf-8');
+        let array_product;
+        if (content) {
+            array_product = JSON.parse(content)
+        }
+        console.log(array_product)
+    }
+
+    /** Se elimina por ID */
+    async deleteById(id) {
+        try {
+            let content = fs.readFileSync('./file/producto.txt', 'utf-8');
+            let array_product;
+            if (content) {
+                array_product = JSON.parse(content)
+            }
+            array_product.splice(id - 1, 1)
+            let array_product_string = JSON.stringify(array_product)
+            await fs.promises.writeFile('./file/producto.txt', array_product_string)
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    /** Se elimina todo */
+    deleteAll() {
+        fs.writeFileSync('./file/producto.txt', '');
+        console.log('Se elimina contenido del archivo');
+    }
+}
+
+/*** Sección de test */
+const container = new Container();
+// container.save({ title: 'rosas', total: 13000, image: 'image/jazmin.lpg' });
+// container.getById(3)
+// container.getAll()
+container.deleteById(2);
+// container.deleteAll();
