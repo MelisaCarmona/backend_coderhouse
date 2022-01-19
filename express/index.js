793656f5a011d876093e541a18c1e3e464816485
melicaso1624 <melicarmona35@gmail.com>
2022-01-18 23:28:09 -0500

    :sparkles:

--- express/index.js
+++ express/index.js
@@ -1,3 +1,13 @@
 const express = require('express')
 const app = express()
 const PORT = 8080
+
+
+app.get('/productos', (req, res) => {
+    res.send(`Número de visitas ${visitas++}`)
+
+})
+
+app.listen(port, () => {
+    console.log(`Salida por puerto ${port}`)
+})
